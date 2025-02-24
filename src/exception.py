2f188dab44c4125448059fb9d8dd021274dad21a
MitaliGhadigaonkar11 <mitalighadigaonkar27@gmail.com>
2025-02-24 14:35:37 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,19 @@
+import sys 
+
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()   #gets information about the exception that occurred. exc_tb is a  is the traceback object, which contains details about where the error happened.
+    
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error Occured In Python Script Name [{0}] Line Number [{1}] Error Message[{2}]".format(
+        file_name, exc_tb.tb_lineno,str(error)
+    )
+    return error_message
+    
+class CustomException(Exception):
+    def __init__(self,error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message  = error_message_detail(error_message, error_detail=error_detail)
+        
+    def __str__(self):
+        return self.error_message
